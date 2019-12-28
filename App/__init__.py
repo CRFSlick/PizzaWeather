d2f3e76babc5174180dba4ca9540278caf32fde3
CRFSlick <47066085+CRFSlick@users.noreply.github.com>
2019-12-27 19:58:10 -0600

    Update to project structure

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
-from App.config import Config
+import json
 
 app = Flask(__name__)
+
+
+class Config:
+    with open(f'{app.root_path}\config\config.json') as config_file:
+        config = json.load(config_file)
+
+    OPENWEATHERMAP_API_KEY = config.get('openweathermap_api_key')
+    RAPID_API_KEY = config.get('rapid_api_key')
+
+
+# Configure the application with API keys
 app.config.from_object(Config)
 
+# Register application routes
 from App.routes.routes import main
-
 app.register_blueprint(main)
