f23564227998f024e1c07b375231fb358b2996ac
CRFSlick <47066085+CRFSlick@users.noreply.github.com>
2019-12-27 20:53:14 -0600

    Added docstrings

--- App/modules/UV_Index/UV_Index.py
+++ App/modules/UV_Index/UV_Index.py
@@ -3,67 +3,114 @@ import requests
 
 
 class UVI(object):
-    _appid = None
+    _api_key = None
     _lat = None
     _lon = None
     _cnt = None
 
-    def __init__(self, appid, lat, lon, cnt=None):
+    def __init__(self, api_key, lat, lon, cnt=None):
 
-        self.appid = appid
+        self.api_key = api_key
         self.lat = lat
         self.lon = lon
         self.cnt = cnt
 
     @property
-    def appid(self):
-        return self._appid
+    def api_key(self):
+        """
+        Returns:
+            api_key (str)
+        """
+        return self._api_key
 
-    @appid.setter
-    def appid(self, value):
-        self._appid = str(value)
+    @api_key.setter
+    def api_key(self, value):
+        """
+        Returns:
+            api_key (str)
+        """
+        self._api_key = str(value)
 
     @property
     def lat(self):
+        """
+        Returns:
+            lat (float)
+        """
         return self._lat
 
     @lat.setter
     def lat(self, value):
+        """
+        Returns:
+            lat (float)
+        """
         self._lat = float(value)
 
     @property
     def lon(self):
+        """
+        Returns:
+            lon (float)
+        """
         return self._lon
 
     @lon.setter
     def lon(self, value):
+        """
+        Returns:
+            lon (float)
+        """
         self._lon = float(value)
 
     @property
     def cnt(self):
+        """
+        Returns:
+            cnt (float)
+        """
         return self._cnt
 
     @cnt.setter
     def cnt(self, value):
+        """
+        Returns:
+            cnt (float)
+        """
         self._cnt = float(value)
 
     @property
     def __base_url(self):
+        """
+        Returns:
+            base_url (str)
+        """
         return "http://api.openweathermap.org/data/2.5/uvi/forecast"
 
     @property
     def __params(self):
-        return {'appid':{self.appid}, 'lat':{self.lat}, 'lon':{self.lon}}
-
-    @property
-    def requests(self):
-        url = f'{self.__base_url}?'
-        for key in self.__params:
-            url += f'{key}{self.__params[key]}'
-        return url
+        """
+        Returns:
+            params (dict)
+        """
+        return {'api_key': {self.api_key}, 'lat': {self.lat}, 'lon': {self.lon}}
 
     @staticmethod
     def __validate_float(input, name, float):
+        """
+        Validates an argument to be a float
+
+        Args:
+            input (float/list)
+            name (str)
+            float (object)
+
+        Returns:
+            input (float/list)
+
+        Raises:
+            ValueError
+        """
         if isinstance(float, list):
             if input == float:
                 return input
@@ -75,6 +122,20 @@ class UVI(object):
 
     @staticmethod
     def __validate_string(input, name, str):
+        """
+        Validates an argument to be a str
+
+        Args:
+            input (str/list)
+            name (str)
+            str (object)
+
+        Returns:
+            input (str/list)
+
+        Raises:
+            ValueError
+        """
         if isinstance(str, list):
             if input == str:
                 return input
@@ -85,9 +146,25 @@ class UVI(object):
             raise ValueError(f"expected '{name}' to be a string value, but got '{input}' instead.")
 
     def __get_UV_Index(self):
-        return json.loads(requests.get(url=self.__base_url,
-                          params=self.__params).text)
+        """
+        Gets UV index data
+
+        Returns:
+            response (dict)
+
+        Raises:
+            Exception
+        """
+        response = json.loads(requests.get(url=self.__base_url, params=self.__params).text)
+        if response.cod == '200':
+            return response
+        else:
+            raise Exception(f'{response.cod}: {response.message}')
 
     def get_json(self):
+        """
+        Returns:
+            uvi (dict)
+        """
         uvi = self.__get_UV_Index()
         return uvi
